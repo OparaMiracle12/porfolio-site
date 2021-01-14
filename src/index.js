3e66487eca67cb87b37ccefe86d6c237c682c278
opara miracle <miracleopara99@gmail.com>
2021-01-14 15:10:28 -0800

    changing bootstrap js settings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import '../node_modules/bootstrap/js/dist/carousel';
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 import './sass/main.scss';
-
-if(module.hot) {
-    module.hot.accept();
-}
